abbeee063ca97c3b76e5f9eb9c169af7a335ddbf
carlospardoo <28719833+carlospardoo@users.noreply.github.com>
2023-06-28 13:39:15 -0500

    ejercicio creacion cookies

---
+++ ManejoCookies/src/main/java/web/CookiesServlet.java
@@ -0,0 +1,44 @@
+
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CookiesServlet")
+public class CookiesServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException{
+        boolean nuevoUsuario = true;
+        
+        Cookie[] cookies = request.getCookies();
+        
+        if(cookies != null){
+            for(Cookie c : cookies){
+                if(c.getName().equals("visitanteRecurrente") && c.getValue().equals("si")){
+                    nuevoUsuario = false;
+                    break;
+                }
+            }
+        }
+        
+        String mensaje;
+        if(nuevoUsuario){
+            Cookie visitanteCookie = new Cookie("visitanteRecurrente", "si");
+            response.addCookie(visitanteCookie);
+            mensaje = "Gracias por visitar nuestro sitio por primera vez";
+        }
+        else{
+            mensaje = "Gracias por visitar nuevamente nuestro sitio";
+        }
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("Mensaje: " + mensaje);
+    }
+    
+}
